703af1877b014531e3df9f283e8770df3a6b8e85
mahya rasa <93031218+mahya-rasa@users.noreply.github.com>
2022-01-15 11:31:43 -0800

    condition

---
+++ Sat-15-JAN-2022-js1/condition.js
@@ -0,0 +1,26 @@
+// if else
+//switch case
+
+let money = 2;
+
+//if(Boolean)
+
+// String(23);
+// Number("mahya");
+
+if (money) {
+  console.log("you are rich!!!");
+} else {
+  console.log("you are poor");
+}
+
+// console.log(Boolean(""));
+// console.log(Boolean(0));
+// console.log(Boolean(2));
+// console.log(Boolean([]));
+// console.log(Boolean({}));
+
+console.log(Boolean("0"));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+console.log(Boolean(null));

6dab2da6c4a0622b5c936f18e8e55ce0ea93a4c5
mahya rasa <93031218+mahya-rasa@users.noreply.github.com>
2022-01-15 09:20:06 -0800

    operator precedence

---
+++ Sat-15-JAN-2022-js1/Strings.js
@@ -0,0 +1,15 @@
+//string
+//""   ''  ``
+
+let str = "hi guys";
+
+console.log(str);
+
+//string methods
+
+console.log(str.length);
+
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[2]);
+console.log(str[3]);

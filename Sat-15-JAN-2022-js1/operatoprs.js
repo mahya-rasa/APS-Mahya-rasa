b332cbad680009002ffc2ed1ec3a92a5e82c45b6
mahya rasa <93031218+mahya-rasa@users.noreply.github.com>
2022-01-15 08:13:41 -0800

    math operators in js

--- Sat-15-JAN-2022-js1/operatoprs.js
+++ Sat-15-JAN-2022-js1/operatoprs.js
@@ -5,3 +5,31 @@
 
 //math
 // +  -  ++  --  *  /   %  **
+
+const now = 2022;
+const ageShekofe = now - 2003;
+const ageMahdie = now - 2003;
+console.log(ageShekofe, ageMahdie);
+
+console.log(ageShekofe * 2);
+
+let id = 1;
+id++; //2
+id--; //1
+id++; //id =id+1
+id * 2;
+id % 2;
+2 ** 3; //8
+
+//NOTE : + in math and string
+
+const firstName = "mahya";
+const lastName = "rasa";
+console.log(firstName + " " + lastName);
+
+console.log(5 + null);
+console.log("10" + null);
+console.log(5 + 3);
+console.log(5 + "3");
+console.log("5" + 2);
+console.log("5" - 2);

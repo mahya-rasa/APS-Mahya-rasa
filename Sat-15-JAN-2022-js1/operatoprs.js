9528d3e237a4e98750f2cee6228ea568477a3e41
mahya rasa <93031218+mahya-rasa@users.noreply.github.com>
2022-01-15 08:30:35 -0800

    operators

--- Sat-15-JAN-2022-js1/operatoprs.js
+++ Sat-15-JAN-2022-js1/operatoprs.js
@@ -6,6 +6,7 @@
 //math
 // +  -  ++  --  *  /   %  **
 
+/*
 const now = 2022;
 const ageShekofe = now - 2003;
 const ageMahdie = now - 2003;
@@ -33,3 +34,35 @@ console.log(5 + 3);
 console.log(5 + "3");
 console.log("5" + 2);
 console.log("5" - 2);
+*/
+
+//assignment operators
+// =   +=   -=   /=  *=   **=   %=
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+// ==   ===    !=  !==    >  <    <= >= : Boolean
+// let pofakPrice = 2000;
+// let chipsPrice = 3000;
+// console.log(pofakPrice < chipsPrice);
+// console.log(pofakPrice > chipsPrice);
+
+//logical
+// &&   ||   !
+
+let pofakPrice = 2000;
+let chipsPrice = 3000;
+
+let sodaPrice = 20000;
+let juicePrice = 3000;
+//and
+//or
+console.log(!(pofakPrice < chipsPrice) && juicePrice < sodaPrice);
+console.log(pofakPrice < chipsPrice || juicePrice > sodaPrice);

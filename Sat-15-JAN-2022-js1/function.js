7f75eb94e6028c32da97113421705d4d28e5473e
mahya rasa <93031218+mahya-rasa@users.noreply.github.com>
2022-01-15 16:33:17 -0800

    function

---
+++ Sat-15-JAN-2022-js1/function.js
@@ -0,0 +1,66 @@
+//function
+
+//the piece of code that we can reuse over and over again
+
+//clean code    don't repeat your self
+
+function logger() {
+  console.log("you are very nice❤️");
+}
+//call innoke execute run
+
+// logger();
+// logger();
+// logger();
+// logger();
+// logger();
+
+//parameter argument   return
+
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+
+// sum(2, 3);
+// sum("mahya ", "rasa");
+// sum(53858, 123);
+
+//pop()  //remove end item from array
+
+function popDuplicate(array) {
+  const newarr = [];
+  for (let i = 0; i < array.length - 1; i++) {
+    newarr.push(array[i]);
+  }
+  return newarr;
+}
+
+let arr = [1, 2, 3, 4, 5, 6];
+
+console.log(popDuplicate(arr));
+
+//statment and expression
+
+//function declaration   regular function
+//function expression
+//arrow function  ES6  FE -shorter faster to write
+
+function calcAge(birhYear) {
+  return 2022 - birhYear;
+}
+
+// const age = function (birhYear) {
+//   return 2022 - birhYear;
+// };
+
+// console.log(age(2010));
+
+const age = (birhYear) => 2022 - birhYear;
+
+console.log(age(2010));
+
+const sum = (x, y) => {
+  x + y;
+};
+
+console.log(sum(10, 12));

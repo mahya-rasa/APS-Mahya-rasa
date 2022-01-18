3a8f20a76d40fb5261c6fca9287c57ef575709f7
mahya rasa <93031218+mahya-rasa@users.noreply.github.com>
2022-01-18 07:41:55 -0800

    primitive and objects

---
+++ Sun-16_JAN-2022-js1/primitiveAndObjects.js
@@ -0,0 +1,19 @@
+let fname = "mahya";
+
+let fname2 = fname;
+
+fname2 = "mahsa";
+
+console.log(fname);
+console.log(fname2);
+
+const car = {
+  color: "blue",
+};
+
+const car2 = car;
+
+console.log(car2.color);
+car2.color = "red";
+console.log(car);
+console.log(car2);
